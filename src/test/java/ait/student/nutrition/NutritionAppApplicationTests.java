57c46ca9f6ec6328ebbf1920049e75abd746d140
shiva466 <37341802+shiva466@users.noreply.github.com>
2023-04-24 03:40:50 +0530

    Update NutritionAppApplicationTests.java

--- src/test/java/ait/student/nutrition/NutritionAppApplicationTests.java
+++ src/test/java/ait/student/nutrition/NutritionAppApplicationTests.java
@@ -1,13 +1,20 @@
 package ait.student.nutrition;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class NutritionAppApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() 
+	{
+		ApplicationContext context = SpringApplication.run(NutritionAppApplication.class);
+	    assertThat(context).isNotNull();	
 	}
 
 }
